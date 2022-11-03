2cbad844c63186ded4ed3dbaa46a778e28ac1bff
xReB3L <87698845+xReB3L@users.noreply.github.com>
2022-11-03 23:41:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const aesjs = require('aes-js');
 const PDFDocument = require('pdf-lib').PDFDocument;
 const fs = require('fs');
 const sanitize = require("sanitize-filename");
-// const md5 = require('md5');
-
 
 let key = new Uint8Array([30, 0, 184, 152, 115, 19, 157, 33, 4, 237, 80, 26, 139, 248, 104, 155]);
 
@@ -14,8 +12,6 @@ async function downloadAndDecryptFile(url) {
     
     return new Promise(async (resolve, reject) => {
         let file = await fetch(url, {method: "GET"}).then(res => res.buffer());
-
-        //console.log(file);
 
         try {
             let header = msgpack.decode(file.slice(0, 256));
@@ -27,7 +23,6 @@ async function downloadAndDecryptFile(url) {
             var aesCbc = new aesjs.ModeOfOperation.cbc(key, firstPart.slice(0, 16));
             var decryptedFirstPart = aesCbc.decrypt(firstPart.slice(16));
 
-            // this mess is to fix what i belive to be a bug in the aes js module
             for(let i=16;i>0;i--){
                 if (decryptedFirstPart.slice(decryptedFirstPart.length-i).every(e=>e==i)) {
                     decryptedFirstPart = decryptedFirstPart.slice(0, decryptedFirstPart.length-i);
